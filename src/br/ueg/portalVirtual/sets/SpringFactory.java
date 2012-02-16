5d598b56b3d9af63b993f24925ca1765d1d08781
Alexandre Tavares <tm.alexandre@gmail.com>
2012-02-16 19:55:22 -0300

    Spring factory.

--- src/br/ueg/portalVirtual/sets/SpringFactory.java
+++ src/br/ueg/portalVirtual/sets/SpringFactory.java
@@ -5,7 +5,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringFactory extends ClassPathXmlApplicationContext {
 	private static ClassPathXmlApplicationContext instance = null;
 	
-	
 	public static ClassPathXmlApplicationContext getInstance() {
 		if (instance == null) {
 			instance = new ClassPathXmlApplicationContext("br/ueg/portalVirtual/sets/spring.xml");
